40a2ceb33ca2f81987fa20016ad7c4271496e62e
Gonzalo Casas <casas@arch.ethz.ch>
2023-07-09 22:53:27 +0200

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ optional_requirements = {}
 
 setup(
     name="compas_eve",
-    version="0.1.0",
+    version="0.1.1",
     description="COMPAS Event Extensions: adds event-based communication infrastructure to the COMPAS framework.",
     long_description=long_description,
     long_description_content_type="text/markdown",
