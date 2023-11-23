27fc6068d2bbf94dfa9f8194616b0576fa5c8466
Gonzalo Casas <casas@arch.ethz.ch>
2023-11-24 00:49:51 +0100

    Bump version to 0.3.2

--- src/compas_eve/ghpython/components/Ce_BackgroundTask/code.py
+++ src/compas_eve/ghpython/components/Ce_BackgroundTask/code.py
@@ -3,7 +3,7 @@ Background Task component.
 
 Executes a long-running task in the background, while keeping Grasshopper reactive.
 
-COMPAS EVE v0.3.1
+COMPAS EVE v0.3.2
 """
 import threading
 import scriptcontext as sc
